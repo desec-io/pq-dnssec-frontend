e89af501ac8ca789a62261786fb7f6307c9626ea
Peter Thomassen <peter@desec.io>
2024-06-05 20:22:09 +0200

    Add contact button

--- src/App.vue
+++ src/App.vue
@@ -8,14 +8,14 @@
 
       <v-spacer></v-spacer>
 
-      <!--v-btn
-        href="https://"
+      <v-btn
+        href="mailto:pqc@desec.io"
         target="_blank"
         text
       >
-        <span class="mr-2">Talk at ...</span>
+        <span class="mr-2">Contact</span>
         <v-icon>mdi-open-in-new</v-icon>
-      </v-btn-->
+      </v-btn>
     </v-app-bar>
 
     <v-main>
