82e2007be8fb80fc0373780948e3d06eba488dde
Peter Thomassen <peter@desec.io>
2024-06-26 23:36:47 +0200

    Add ICANN 80 talk

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,15 @@
       <v-spacer></v-spacer>
 
       <v-btn
+          href="https://static.sched.com/hosted_files/icann80/11/2.6%20Peter%20Thomassen%202024-06-10%20ICANN%2080%2C%20Field%20Experiments%20on%20Post-Quantum%20DNSSEC.pdf"
+          target="_blank"
+          text
+      >
+        <span class="mr-2">Preliminary Results at ICANN 80</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+
+      <v-btn
         href="mailto:pqc@desec.io"
         target="_blank"
         text
